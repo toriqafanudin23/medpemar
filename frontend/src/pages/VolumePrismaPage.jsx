24f8886f0a69a6a79c24d5241ea3e58055d376b1
toriqafanudin23 <toriqafanudin23@gmail.com>
2026-04-19 10:10:11 +0700

    -

--- frontend/src/pages/VolumePrismaPage.jsx
+++ frontend/src/pages/VolumePrismaPage.jsx
@@ -32,8 +32,20 @@ const VolumePrismaPage = () => {
       <ImageDisplay src="prismasegilima.png" scale={0.7} nama="Gambar 10. Prisma Segilima" />
       <ImageDisplay src="prismasegi8.png" scale={0.7} nama="Gambar 11. Prisma Segidelapan" />
 
-      <Paragraph>Tentu masih banyak jenis prisma lainnya. Bayangkan setiap bangun yang memiliki dua sisi, alas dan tutup yang sama, kemudian kedua sisi dihubungkan dengan persegi panjang, bisa kita sebut sebagai prisma. Nah sekarang coba kamu perhatikan bangun pada <MathInline>{`\\text{Gambar 12}`}</MathInline>, apakah bisa disebut prisma?</Paragraph>
+      <Paragraph>Tentu masih banyak jenis prisma lainnya. Bayangkan setiap bangun yang memiliki dua sisi, alas dan tutup yang sama, kemudian kedua sisi dihubungkan dengan persegi panjang, bisa kita sebut sebagai prisma. Nah sekarang coba kamu perhatikan bangun pada <MathInline>{`\\text{Gambar 12}`}</MathInline>, apakah bisa disebut sebagai prisma?</Paragraph>
       <ImageDisplay src="tabung.png" scale={0.7} nama="Gambar 12. Tabung" />
+      <Heading3>Volume Prisma</Heading3>
+      <Paragraph>Sebelum menghitung volume prisma, kamu perlu mengingat kembali cara mencari volume kubus dan balok. Volume kubus dapat dicari dengan</Paragraph>
+      <MathBlock>{`V = r \\times r \\times r`}</MathBlock>
+      <Paragraph> Sedangkan volume balok dapat dicari dengan</Paragraph>
+      <MathBlock>{`V = p \\times l \\times t`}</MathBlock>
+      <Paragraph>Jika kamu mengamati volume kubus dan balok, keduanya memiliki kesamaan, yaitu cara mencari volumenya adalah dengan mengalikan luas alas dengan tinggi.</Paragraph>
+      <MathBlock>{`V = \\text{Luas Alas} \\times \\text{Tinggi}`}</MathBlock>
+      <Paragraph>Dimana <MathInline>{`r \\times r`}</MathInline> merupakan luas alas dari kubus, sedangkan <MathInline>{`p \\times l`}</MathInline> merupakan luas alas dari balok. Sehingga dapat disimpulkan bahwa bangun yang memiliki sisi alas dan tutup yang kongruen dan dihubungkan dengan persegi panjang bisa disebut sebagai prisma. Mudahnya, kubus dan balok adalah prisma segiempat.</Paragraph>
+      <Paragraph>Volume prisma secara umum dapat dicari dengan</Paragraph>
+      <MathBlock>{`V = \\text{Luas Alas} \\times \\text{Tinggi}`}</MathBlock>
+      <Paragraph>Dimana alas prisma bisa berbentuk segitiga, segi lima, segi delapan, atau bentuk lainnya.</Paragraph>
+
 
 
 
