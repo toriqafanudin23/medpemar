27288f3f3fc8f877ab1892ed0ff1e0ea73111979
toriqafanudin23 <toriqafanudin23@gmail.com>
2026-04-25 20:13:47 +0700

    luas limas

--- frontend/src/pages/LuasLimasPage.jsx
+++ frontend/src/pages/LuasLimasPage.jsx
@@ -20,18 +20,25 @@ const LuasLimasPage = () => {
       <Heading2>D. Luas Permukaan Limas</Heading2>
       <Heading3>Jaring-jaring Limas</Heading3>
 
-      <Paragraph>Jaring-jaring yang akan dipelajari terakhir kali adalah limas. Jaring-jaring limas tersusun dari alas berbentuk segi-<MathInline>{`n`}</MathInline> dan segitiga sejumlah <MathInline>{`n`}</MathInline>. Kamu bisa 
-        melihat contoh jaring-jaring limas segiempat pada <MathInline>{`\\text{Objek 9}`}</MathInline>.</Paragraph>
+      <Paragraph>Jaring-jaring yang akan dipelajari terakhir kali adalah limas. Jaring-jaring limas tersusun dari alas berbentuk segi-n dan segitiga sejumlah n. Kamu bisa 
+        melihat contoh jaring-jaring limas segiempat pada <MathInline>{`\\text{Objek 9}`}</MathInline> berikut.</Paragraph>
 
       <Viewer3D
         modelPath={MODELS.JARING_LIMAS}
         urlAR={AR_URLS.LIMAS_JARING}
-        scale={0.6}
+        scale={0.5}
         title="Objek 9. Jaring-jaring Limas Segiempat"
       />
       <ImageDisplay src="jaringlimassegi3.png" />
       <ImageDisplay src="jaringlimassegi4.png" />
       <ImageDisplay src="jaringlimassegi6.png" nama="Gambar 25. Contoh Jaring-jaring Limas" />
+      <ImageDisplay src="jaringlimaspuncakt.png" nama="Gambar 26. Jaring-jaring Limas Puncak Tidak di Tengah" />
+      <Paragraph>Tentu masih banyak lagi contoh jaring-jaring limas yang lain. Dalam materi ini terbatas pada limas dengan alas berbentuk segi-n beraturan. Sebagai latihan, cobalah kamu buat jaring-jaring limas segiempat selain yang sudah dicontohkan!</Paragraph>
+      <Heading3>Luas Permukaan Limas</Heading3>
+      <Paragraph>Luas permukaan limas dapat dilihat dari jaring-jaringnya. Pada limas segi-n beraturan luas permukaannya dapat dicari dengan</Paragraph>
+      <MathBlock>{`L=\\text{Luas Alas}+n \\times \\text{Sisi Tegak}`}</MathBlock>
+      <Paragraph>Dalam hal ini sisi tegak limas berbentuk segitiga.</Paragraph>
+
 
 
 
